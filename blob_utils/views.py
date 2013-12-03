6e3f52a4abf6547e1e8839881696f69707060517
georgefs <goblin.george@gmail.com>
2013-12-03 19:53:59 +0800

    update 2.5

--- blob_utils/views.py
+++ blob_utils/views.py
@@ -1,11 +1,19 @@
-import webapp2
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import blobstore_handlers
+import logging
+
 
 class Upload(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
+        logging.info('test')
         blob_key = str(self.get_uploads('default')[0].key())
+        logging.info(blob_key)
         self.response.write(blob_key)
 
-app = webapp2.WSGIApplication([
+app = webapp.WSGIApplication([
     ('.*',  Upload),
 ], debug=True)
+
+if __name__ == "__main__":
+    run_wsgi_app(app)
